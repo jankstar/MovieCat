bb7cd962254fe1ff05663a569e9c68cd644aeeff
jankstar <jankstar.berlin@gmail.com>
2023-04-08 08:21:37 +0200

    remove webRTC lib

--- main.go
+++ main.go
@@ -136,7 +136,7 @@ func main() {
 	// Global middleware
 	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
 	// By default gin.DefaultWriter = os.Stdout
-	r.Use(gin.Logger())
+	//r.Use(gin.Logger())
 
 	// Recovery middleware recovers from any panics and writes a 500 if there was one.
 	r.Use(gin.Recovery())
